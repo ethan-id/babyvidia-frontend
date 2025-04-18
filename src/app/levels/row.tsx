cd81ec837972bc20b313a837754667bb4e91ba8b
ehancock <ehancock@iastate.edu>
2025-04-18 12:57:22 -0500

    Labels for /levels

--- src/app/levels/row.tsx
+++ src/app/levels/row.tsx
@@ -9,9 +9,9 @@ interface RowProps {
 
 export const Row: FC<RowProps> = ({building, room, capacity}) => (
     <div className='flex border p-2 gap-12 items-center border-slate-200 min-h-[50px] rounded-xl'>
-        <p className='font-bold'>{building}</p>
-        <p className='font-bold'>{room}</p>
-        <p>{capacity}</p>
+        <p className='font-bold'>Building: {building}</p>
+        <p className='font-bold'>Room: {room}</p>
+        <p>Capacity: {capacity}</p>
         <Link href={'/levels/change-me'}>Click me to see route params example</Link>
     </div>
 )

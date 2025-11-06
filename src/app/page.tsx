0682de740245f704ddfb98ab757371e59291c77b
ehancock <ehancock@iastate.edu>
2025-11-06 11:51:02 -0600

    Load home page even if web server is down

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,19 @@ const jetsonBaseURL = process.env.JETSON_URL;
 const fetchMessage = async () => {
     const url = new URL('/hello', jetsonBaseURL);
 
-    const res = await fetch(url.toString());
-    if (!res.ok) {
-        throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
-    }
+    try {
+        const res = await fetch(url.toString());
+        if (!res.ok) {
+            // throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
+            return null;
+        }
 
-    const data: string = await res.text();
-    return data;
+        const data: string = await res.text();
+
+        return data;
+    } catch {
+        return null;
+    }
 };
 
 export default async function Home() {
@@ -19,7 +25,7 @@ export default async function Home() {
 
     return (
         <main className='flex flex-col gap-6 min-h-screen items-center justify-center p-4'>
-            <h1 className='text-2xl'>{message ?? 'Loading…'}</h1>
+            <h1 className='text-2xl'>Server status: {message ? <span className='text-green-500'>Online</span> : <span className='text-red-500'>Offline</span>}</h1>
             <Link
                 href='/levels'
                 className='mt-6 inline-block px-4 py-2 bg-blue-600 text-white rounded-sm hover:bg-blue-700'
